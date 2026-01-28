d9ecc7f1ac3a1834cf8b2e43d35f787f99516fdc
Robert-Lexicon <robert.hedblad@lexicon.se>
2026-01-28 11:14:22 +0100

    Added grid example with children

--- components/ui/card-grid.tsx
+++ components/ui/card-grid.tsx
@@ -1,22 +1,19 @@
 import Image from "next/image";
 import { Character } from "@/types/futurama";
+import CharacterCard from "@/components/ui/character-card";
 
 //export default function CardGrid(props: { characters: Character[] }) {
 //const characters = props.characters
 //const { characters } = props
 
 //our type for the props in this case  : { characters: Character[] }
-export default function CardGrid({ characters }: { characters: Character[] }) {
+export default function CardGrid({ title, children }: { title: string, children: React.ReactNode }) {
     return (
-        <section>
-            <h2>Characters</h2>
+        <section className="container mx-auto px-4 py-16 space-y-4">
+            <h2 className="text-3xl font-bold font-josefin">{title}</h2>
             <ul className="grid grid-cols-[repeat(auto-fill,minmax(35ch,1fr))] gap-4">
                 {
-                    characters.map((character) => (
-                        <li key={character.id}>
-                            <h3>{character.name}</h3>
-                            <Image className="w-full" src={character.image ?? "/placeholder.png"} width={100} height={100} alt={character.name} />
-                        </li>))
+                    children
                 }
 
             </ul>
