9a2e60a5e2dcf97da9cb7b998c441942eef7e74b
Robert-Lexicon <robert.hedblad@lexicon.se>
2026-02-11 10:09:00 +0100

    feat: add `SearchForm` component for character search with clear functionality and URL query management.

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,18 +1,62 @@
-import Form from 'next/form'
+"use client";
+
+import Form from "next/form";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useRef } from "react";
 
 // This component is a form that will search for a character in the API
 // It uses the Form component from next/form to handle the form submission
 // It uses the searchParams to get the query from the URL
 // It uses the getCharacters function from data/character to get the characters
 
-//TODO: make it possible to pick up the query from the URL and use it in the search and also make it possible to clear the search
-
+// Added a clear button that will clear the search and remove the query from the URL
+// Added a ref to the form to be able to reset it
 export default function SearchForm() {
+    const searchParams = useSearchParams();
+    const { replace } = useRouter();
+    const pathname = usePathname();
+    const ref = useRef<HTMLFormElement>(null);
+    const query = searchParams.get("query");
+
+    // Clear search and remove query from URL
+    // We use a ref to access the form element directly to call the reset() method
+    // which clears all form inputs. This is simpler than managing state for the input.
+    const handleClear = () => {
+        if (ref.current) {
+            ref.current.reset();
+        }
+        const params = new URLSearchParams(searchParams);
+        params.delete("query");
+        replace(`${pathname}?${params.toString()}`);
+    };
+
     return (
-        <Form action="/" className="flex gap-2">
-            <label className="sr-only" htmlFor="search">Search:</label>
-            <input className="border text-inherit dark:bg-neutral-900" id="search" name="query" placeholder="Search..." />
-            <button className="border text-inherit dark:bg-neutral-900 cursor-pointer px-4 py-1" type="submit">Search</button>
+        <Form action="/" className="flex gap-2" ref={ref}>
+            <label className="sr-only" htmlFor="search">
+                Search:
+            </label>
+            <input
+                className="border text-inherit dark:bg-neutral-900"
+                id="search"
+                name="query"
+                placeholder="Search..."
+                defaultValue={query || ""}
+            />
+            {query && (
+                <button
+                    className="border text-inherit dark:bg-neutral-900 cursor-pointer px-4 py-1"
+                    type="button"
+                    onClick={handleClear}
+                >
+                    Clear
+                </button>
+            )}
+            <button
+                className="border text-inherit dark:bg-neutral-900 cursor-pointer px-4 py-1"
+                type="submit"
+            >
+                Search
+            </button>
         </Form>
-    )
+    );
 }
